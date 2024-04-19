f193ac7863d2afa8f4189a3a8b4b6cbc71294a94
Mathispnz <Mathis.peingnez@gmail.Com>
2024-04-19 17:42:15 +0200

    Search results added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,19 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+if(module.hot) {
+  module.hot.accept();
+}
+
 const controlRecipes = async function() {
   try {
-    const id = '5ed6604591c37cdc054bc90b';
+    const id = window.location.hash.slice(1);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -17,13 +24,30 @@ const controlRecipes = async function() {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch(err) {
+    recipeView.renderError();
+  }
+}
+
+const controlSearchResults = async function() {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.state.search.results);
+  } catch(err) {
     console.log(err);
   }
 }
 
-// window.addEventListener('hashchange', controlRecipes)
-// window.addEventListener('load', controlRecipes)
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults)
 }
 init();
